005b6a00b022e7b950061d0fcd7274499d5e8d5e
zhangqiongwen <zhangqiongwen@cleartv.cn>
2020-10-21 18:09:57 +0800

    加入FileManger和GameMaster项目

---
+++ FlieManager/src/main/java/com/example/fliemanager/FileManagerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fliemanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FileManagerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_file_manager)
+    }
+}
---
+++ GameMaster/src/main/java/com/example/gamemaster/GameMasterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gamemaster
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameMasterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_master)
+    }
+}
