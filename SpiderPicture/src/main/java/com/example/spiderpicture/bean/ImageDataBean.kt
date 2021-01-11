278de8e3b546e8cd5dbf2a28130975f35659e776
zhangqiongwen <1796874227@qq.com>
2021-01-11 08:12:57 +0800

    1、更新gradle 2、更新部分解析的代码

--- SpiderPicture/src/main/java/com/example/spiderpicture/bean/ImageDataBean.kt
+++ SpiderPicture/src/main/java/com/example/spiderpicture/bean/ImageDataBean.kt
@@ -3,16 +3,15 @@ package com.example.spiderpicture.bean
 import android.graphics.Bitmap
 import androidx.annotation.Nullable
 
-class ImageDataBean(var innerUrl: String, var coverImageUrl: String, @Nullable var bitmapCover: Bitmap?, @Nullable var bitmapInner: Bitmap?){
+class ImageDataBean(var innerUrl: String, var coverImageUrl: String, @Nullable var bitmapCover: Bitmap?, var tittle: String){
 
-    constructor(innerUrl: String, coverImageUrl: String): this(innerUrl, coverImageUrl, null, null)
+    constructor(innerUrl: String, coverImageUrl: String, tittle: String): this(innerUrl, coverImageUrl, null, tittle)
 
     override fun toString(): String {
 
         return "the ImageLiDataBean is : \n httpUrl: ".plus(innerUrl)
             .plus("\n coverImageUrl: ").plus(coverImageUrl)
             .plus("\n the bitmap is null ?").plus(bitmapCover == null)
-            .plus("\n the bitmap inner is null ?").plus(bitmapInner == null)
-
+            .plus("\n the tittle is ").plus(tittle)
     }
 }
--- SpiderPicture/src/main/java/com/example/spiderpicture/util/ResolveUtil.kt
+++ SpiderPicture/src/main/java/com/example/spiderpicture/util/ResolveUtil.kt
@@ -32,30 +32,53 @@ object ResolveUtil{
     fun resolveLevel2AtPosition(position: Int, urlResponse: String): ArrayList<ImageDataBean>{
         when(position){
             0 -> return resolveMNXZ(urlResponse)
-//            1 -> return resolveJDLY(urlResponse)
-//            2 -> return resolveFLJ(urlResponse)
-//            3 -> return resolveTAGS(urlResponse)
-            1 -> return resolveMNXZ(urlResponse)
-            2 -> return resolveMNXZ(urlResponse)
-            3 -> return resolveMNXZ(urlResponse)
+            1 -> return resolveJDLY(urlResponse)
+            2 -> return resolveFLJ(urlResponse)
+            3 -> return resolveTAGS(urlResponse)
+//            1 -> return resolveMNXZ(urlResponse)
+//            2 -> return resolveMNXZ(urlResponse)
+//            3 -> return resolveMNXZ(urlResponse)
             else -> throw Exception("there is no resolve method for the position $position")
         }
     }
 
+
     fun resolveMNXZ(urlResponse: String): ArrayList<ImageDataBean> {
+//        var dataList: ArrayList<ImageDataBean> = ArrayList()
+//        var doc: Document = Jsoup.parse(urlResponse)
+//        var doc_LiList = doc.getElementById("post-list").getElementsByTag("li")
+//        for (i in doc_LiList) {
+//            for (j in i.getElementsByClass("thumb-link")) {
+////                    Log.i(TAG, "resolveMnxz: the j is " + j.getElementsByTag("img")[0].attr("data-src"))
+//                dataList.add(ImageDataBean(j.attr("href"), j.getElementsByTag("img")[0].attr("data-src")))
+//            }
+//        }
+//        Log.i(TAG, "resolveMnxz: the datalist is " + dataList.toString())
+//        return dataList
 
         var dataList: ArrayList<ImageDataBean> = ArrayList()
-        var doc: Document = Jsoup.parse(urlResponse)
-        var doc_LiList = doc.getElementById("post-list").getElementsByTag("li")
-        for (i in doc_LiList) {
-            for (j in i.getElementsByClass("thumb-link")) {
-//                    Log.i(TAG, "resolveMnxz: the j is " + j.getElementsByTag("img")[0].attr("data-src"))
-                dataList.add(ImageDataBean(j.attr("href"), j.getElementsByTag("img")[0].attr("data-src")))
-            }
+
+        var rootDoc: Document = Jsoup.parse(urlResponse)
+        var items = rootDoc.getElementById("page")
+                .getElementById("content")
+                .getElementById("primary-home")
+                .getElementById("post-list")
+                .getElementsByTag("li")
+        for (item in items){
+            var tittleItem = item.getElementsByClass("post-info")
+            var pictureItem = item.getElementsByClass("post-module-thumb b2-radius")
+
+            var coverPictureUrl = pictureItem[0].getElementsByTag("img")[0].attr("data-src")
+            var innerPictureUrl = pictureItem[0].attr("href")
+            var tittle = tittleItem[0].getElementsByTag("h2")
         }
-        Log.i(TAG, "resolveMnxz: the datalist is " + dataList.toString())
+
         return dataList
     }
+
+
+
+
     fun resolveJDLY(urlResponse: String): ArrayList<ImageDataBean>{
         TODO("not done as well")
     }
