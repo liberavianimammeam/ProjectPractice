a798f237a6cdc80ea793537a336c6221e1f05482
zhangqiongwen <1796874227@qq.com>
2021-02-20 21:01:24 +0800

    更新FileManager 调整图片加载的方向使大图加载连贯

--- FlieManager/src/main/java/com/example/fliemanager/ui/PictureActivity.kt
+++ FlieManager/src/main/java/com/example/fliemanager/ui/PictureActivity.kt
@@ -39,7 +39,9 @@ class PictureActivity: AppCompatActivity() {
             this,
             metrics
         )
+
         ap_picture_views.adapter = mAdapter
+        ap_picture_views.rotationY = 180f
 
         Log.i(TAG, "onCreate: the pictureDataNow is ${FileManager.getPictureDataNow()}")
         
@@ -65,12 +67,14 @@ class PictureActivity: AppCompatActivity() {
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
         if (hasFocus){
-            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                     or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
                     or View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    )
         }
 
     }
